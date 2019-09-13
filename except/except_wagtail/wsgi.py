3fd63b29cfe95d3109a8d977569b2019d6fb46bc
tzzat <alexandrefradet0@gmail.com>
2019-09-13 12:54:22 +0200

    change for deployement

--- except/except_wagtail/wsgi.py
+++ except/except_wagtail/wsgi.py
@@ -1,27 +1,15 @@
+
 """
-exposes the WSGI callable as a module-level variable named ``application``. 
-For more information on this file, see 
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/ 
-""" 
-import os 
-import time 
-import traceback 
-import signal 
-import sys 
- 
-from django.core.wsgi import get_wsgi_application 
- 
-sys.path.append('/var/www/html/except.eco') 
-# adjust the Python version in the line below as needed 
-sys.path.append('/var/www/html/except.eco/gray-unicorn/env/lib/python3.5/site-packages') 
- 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "except.settings") 
- 
-try: 
-    application = get_wsgi_application() 
-except Exception: 
-    # Error loading applications 
-    if 'mod_wsgi' in sys.modules: 
-        traceback.print_exc() 
-        os.kill(os.getpid(), signal.SIGINT) 
-        time.sleep(2.5) 
+WSGI config for except_wagtail project.
+It exposes the WSGI callable as a module-level variable named ``application``.
+For more information on this file, see
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "except_wagtail.settings.dev")
+
+application = get_wsgi_application()
