27c43fb8ad907c6e50d53582df3c3ee39faeb80c
tzzat <alexandrefradet0@gmail.com>
2019-09-13 15:54:34 +0200

    change for deployement

--- except/except_wagtail/settings/production.py
+++ except/except_wagtail/settings/production.py
@@ -5,6 +5,11 @@ DEBUG = False
 
 SECRET_KEY = '*d7hN$@<GsVb`y3}|QN"vn5~e2`b5='
 
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
 try:
     from .local import *
 except ImportError:
