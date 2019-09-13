3bf7763028dd6dc25a839b6d33bbdc112ab78b60
tzzat <alexandrefradet0@gmail.com>
2019-09-13 15:57:51 +0200

    change for deployement

--- except/except_wagtail/settings/base.py
+++ except/except_wagtail/settings/base.py
@@ -184,7 +184,7 @@ STATICFILES_FINDERS = [
 ]
 
 STATICFILES_DIRS = [
-    os.path.join(PROJECT_DIR, 'static'),
+    os.path.join(PROJECT_DIR, 'static'),'/var/www/htmk/except.eco/gray-unicorn/except'
 ]
 
 
