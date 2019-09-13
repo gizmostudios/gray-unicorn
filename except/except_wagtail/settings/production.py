97d7e30d4141a8ddc26377f24492b6e23ef042e9
tzzat <alexandrefradet0@gmail.com>
2019-09-13 16:00:54 +0200

    change for deployement

--- except/except_wagtail/settings/production.py
+++ except/except_wagtail/settings/production.py
@@ -5,6 +5,8 @@ DEBUG = False
 
 SECRET_KEY = '*d7hN$@<GsVb`y3}|QN"vn5~e2`b5='
 
+STATIC_URL = '/var/www/htmk/except.eco/gray-unicorn/except/static/'
+
 ALLOWED_HOSTS = ['*'] 
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
