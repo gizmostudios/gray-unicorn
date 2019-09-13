563e318ab72bcfe2530d2c4ed87c8d4ca5d498bd
tzzat <alexandrefradet0@gmail.com>
2019-09-13 13:03:46 +0200

    change for deployement

--- except/except_wagtail/wsgi.py
+++ except/except_wagtail/wsgi.py
@@ -7,9 +7,12 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
+
+sys.path.append('/var/www/html/except.eco/gray-unicorn/except')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "except_wagtail.settings.dev")
 
 application = get_wsgi_application()
