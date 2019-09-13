ad07ae2fc32d8f6402e7ead0b505a73901f60d61
tzzat <alexandrefradet0@gmail.com>
2019-09-13 17:42:21 +0200

    deployement

--- except/except_wagtail/settings/production.py
+++ except/except_wagtail/settings/production.py
@@ -14,6 +14,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # the site admins on every HTTP 500 error when DEBUG=False.
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
+
+ADMINS = [('Alexandre, "alexandre@except.nl")]
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
