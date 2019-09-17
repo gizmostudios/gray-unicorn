f069c2dc5c51396af9227083f8110637f51b036d
tzzat <alexandrefradet0@gmail.com>
2019-09-17 14:40:33 +0200

    update for deployment

--- except/except_wagtail/static/js/calendar.js
+++ except/except_wagtail/static/js/calendar.js
@@ -35,7 +35,6 @@ var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
 var yyyy = today.getFullYear();
 
 today = yyyy + '-' + mm + '-' + dd;
-console.log(today);
 
 document.addEventListener('DOMContentLoaded', function() {
   $.post("/ajax/load_calendar/")
